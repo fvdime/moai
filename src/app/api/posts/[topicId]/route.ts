a6e6c7916f4205da4a2d5d5bc8e8738beb497ab0
Mustafa Kemal Gördesli <m.kemalgordesli@gmail.com>
2023-12-18 20:50:32 +0300

    Add create and delete routes

--- src/app/api/posts/[topicId]/route.ts
+++ src/app/api/posts/[topicId]/route.ts
@@ -2,17 +2,29 @@ import { NextRequest, NextResponse } from 'next/server';
 import httpStatus from 'http-status';
 import error from '@/libs/error-handler';
 import prisma from '@/libs/prisma';
-import { GetAllTopicByPaged } from '@/services/topic';
 
-export async function GET(req: NextRequest) {
+export async function GET(
+    req: NextRequest,
+    { params }: { params: { topicId: string } }
+) {
     try {
+        const { topicId } = params;
         const url = new URL(req.url);
         const page: number = Number(url.searchParams.get('page')) || 1;
         const size: number = Number(url.searchParams.get('size')) || 20;
 
-        const topics = await GetAllTopicByPaged(page, size);
+        const posts = await prisma.post.findMany({
+            where: {
+                topicId: topicId,
+            },
+            skip: (page - 1) * size,
+            take: size,
+            orderBy: {
+                createdAt: 'desc',
+            },
+        });
 
-        return NextResponse.json({ success: true, topics: topics });
+        return NextResponse.json({ success: true, topics: posts });
     } catch (err) {
         return error({
             error: err,
--- src/services/topic.ts
+++ src/services/topic.ts
@@ -7,3 +7,21 @@ export const CreateTopic = async (topic: { title: string; userId: string }) => {
         },
     });
 };
+
+export const GetAllTopicByPaged = async (
+    page: number = 1,
+    size: number = 10,
+    orderBy: string = 'desc'
+) => {
+    return await prisma.topic.findMany({
+        skip: (page - 1) * size,
+        take: size,
+        orderBy: { createdAt: orderBy == 'asc' ? 'asc' : 'desc' },
+    });
+};
+
+export const GetTopic = async (where: { id: string; userId?: string }) => {
+    return await prisma.topic.findFirst({
+        where,
+    });
+};
