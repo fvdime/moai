6227b4c1972c4e690017fce4e13b93194e45a98f
fvdime <73942727+fvdime@users.noreply.github.com>
2024-01-18 11:49:20 +0300

    add post data fetch

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-const withNextIntl = require('next-intl/plugin')('./i18n.ts');
- 
-module.exports = withNextIntl(nextConfig);
+const withNextIntl = require("next-intl/plugin")("./i18n.ts");
+
+module.exports = {
+  ...withNextIntl(nextConfig),
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3.amazonaws.com",
+        port: "",
+        pathname: "/maoiproject.s3.eu-central-1.amazonaws.com",
+      },
+    ],
+  },
+};
--- src/services/post.ts
+++ src/services/post.ts
@@ -20,3 +20,38 @@ export const DeletePost = async (where: { id: string; userId?: string }) => {
         where,
     });
 };
+
+type PipelineStage =
+  | {
+      $search: {
+        index: string
+        text: {
+          query: string
+          fuzzy: {}
+          path: {
+            wildcard: string
+          }
+        }
+      }
+    }
+  | {
+      $skip: number
+    }
+  | {
+      $limit: number
+    }
+    
+export const GetAllPost = async () => {
+   const posts = await prisma.post.findMany()
+   return posts
+}
+
+export const SinglePost = async (id: string) => {
+    const post = await prisma.post.findFirst({
+        where: {
+            id: id
+        }
+    })
+
+    return post
+}
